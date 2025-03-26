93e2a357f9af0094a627de1d2a51be0ea2184ade
sbremer05 <utw5es@virginia.edu>
2025-03-26 13:41:47 -0400

    User can now add topics

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -13,5 +13,8 @@ urlpatterns = [
     path('topics/', views.topics, name = 'topics'),
 
 #     Detail page for a single topic
-    path('topic/<int:topic_id>/', views.topic, name='topic'),
+    path('topic/<int:topic_id>/', views.topic, name = 'topic'),
+
+#     Page for adding a new topic
+    path('new_topic/', views.new_topic, name = 'new_topic'),
 ]
